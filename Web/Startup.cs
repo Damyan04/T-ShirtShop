1baeaa083cec0da51a024deb9db60a485c47e178
Damyan <damo.ts.04@gmail.com>
2019-08-19 02:56:47 +0300

    assebly for migrations change

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -67,6 +67,8 @@ namespace TShirtShop
 
             // using Microsoft.AspNetCore.Identity.UI.Services;
             services.AddSingleton<IEmailSender, EmailSender>();
+            services.AddMvc();
+            services.AddSession();
         }
     
 
@@ -84,7 +86,7 @@ namespace TShirtShop
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseSession();
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
